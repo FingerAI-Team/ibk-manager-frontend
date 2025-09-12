0f0c0cedf41dd83082344f7db9046b1fe0eea4ba
Finger-cori <badzoo@naver.com>
2025-09-12 14:56:14 +0900

    Update daily-chart.tsx
    x-label & chart move to left

--- src/app/components/chat-analytics/daily-chart.tsx
+++ src/app/components/chat-analytics/daily-chart.tsx
@@ -152,10 +152,14 @@ export function DailyChart() {
             </div>
           ) : (
             <ResponsiveContainer width="100%" height="100%">
-              <LineChart data={chartData} className="chart">
+              <LineChart data={chartData} className="chart" margin={{ top: 20, right: 30, left: 0, bottom: 50 }}>
                 <XAxis 
                   dataKey="date" 
                   tickFormatter={(value) => dayjs(value).format('MM/DD')}
+                  angle={-45}
+                  textAnchor="end"
+                  height={50}
+                  interval="preserveStartEnd"
                 />
                 <YAxis />
                 <Tooltip 
