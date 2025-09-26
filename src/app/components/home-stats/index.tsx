f1b57e94cf33a6acc557f7c984fd07e5109f6671
Jaedong95 <ojd9512@gmail.com>
2025-09-26 16:44:24 +0900

    comment update

--- src/app/components/home-stats/index.tsx
+++ src/app/components/home-stats/index.tsx
@@ -93,7 +93,7 @@ export function HomeStats() {
       </div>
       
       <div className="data-collection-notice">
-        * 데이터 수집은 매 시간 5분, 종목 예측은 매 시간 6분에 진행됩니다.
+        * 데이터 수집 및 처리는 매 정시 5분에 진행됩니다.
       </div>
     </div>
   )
