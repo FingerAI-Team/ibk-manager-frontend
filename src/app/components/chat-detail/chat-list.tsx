f574c583bfbc6239c605c79b5877ab1014986e75
Finger-cori <badzoo@naver.com>
2025-10-17 14:50:22 +0900

    대화내역 날짜 표기 오류 fix

--- src/app/components/chat-detail/chat-list.tsx
+++ src/app/components/chat-detail/chat-list.tsx
@@ -38,6 +38,7 @@ export const ChatList: React.FC<ChatListProps> = ({
   const formatDate = (timestamp: string) => {
     const date = new Date(timestamp);
     return date.toLocaleString('ko-KR', {
+      year: 'numeric',
       month: '2-digit',
       day: '2-digit',
       hour: '2-digit',
